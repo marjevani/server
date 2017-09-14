eb01e93aef40c5b5d5fb848b77d722491f403007
Maor <Maor@Maors-MacBook-3.local>
2017-09-14 20:05:00 +0300

    my first test

---
+++ DataBase/create tables.sql
@@ -0,0 +1,42 @@
+﻿
+create table Customer
+(
+id nchar(9) primary key,
+name nvarchar(10) not null,
+pass char(9) not null
+);
+---------------------------
+create table Movie
+(
+id int identity(1,1) primary key,
+name nvarchar(30) not null,
+publish_date date default(getdate()),
+langth smallint check(langth > 0),
+--genre ENUM('פעולה', 'מתח', 'קומדיה', 'דרמה', 'רומנטי', 'אימה') not null
+);
+---------------------------
+create table PlayTime
+(
+id int identity(1,1) primary key,
+movie_id int foreign key references Movie(id),
+play datetime not null,
+total_sits int check(total_sits > 0),
+availble_sits int default(0)
+);
+------------------------
+create table CustomerBuyTickets
+(
+id int identity(1,1) primary key,
+customer_id nchar(9) foreign key references Customer(id),
+movie_id int foreign key references Movie(id),
+amount int check(amount > 0)
+);
+
+
+
+
+
+
+
+
+
