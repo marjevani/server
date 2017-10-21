564f1268cfd21105d8d35294a68413ad1cf2157b
Maor <maor.arjevani@gmail.com>
2017-10-21 13:29:50 +0300

    תיקון הצגות תאריך ושעה

---
+++ ClassLibrary/WebApplication1/html/DaysHeader.js
@@ -0,0 +1,8 @@
+﻿var days = new Array();
+days[days.length] = "ראשון";
+days[days.length] = "שני";
+days[days.length] = "שלישי";
+days[days.length] = "רביעי";
+days[days.length] = "חמישי";
+days[days.length] = "שישי";
+days[days.length] = "שבת";
